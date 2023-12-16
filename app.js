87d9fa9fe5d9ded9c770f7fa7a9d5d18ef4cda61
TosinT <talabitosin67@gmail.com>
2023-12-16 08:21:22 +0100

    rpm calc

---
+++ app.js
@@ -0,0 +1,26 @@
+const N = document.querySelector("#N");
+const cpt = document.querySelector("#cpt");
+const rpm = document.querySelector("#rpm");
+const answer = document.querySelector(".answer");
+const button = document.querySelector(".calculate");
+const input = document.querySelectorAll("input");
+
+function handleInputChange() {
+  const nInt = N.value;
+  const cptInt = cpt.value;
+  const rpmInt = rpm.value;
+  button.addEventListener("click", function () {
+    answer.innerHTML = nInt * cptInt * rpmInt;
+  });
+}
+function resetInputValue() {
+  N.value = NaN;
+  rpm.value = NaN;
+  cpt.value = NaN;
+}
+
+cpt.addEventListener("input", handleInputChange);
+rpm.addEventListener("input", handleInputChange);
+N.addEventListener("input", handleInputChange);
+
+window.onload = resetInputValue;
